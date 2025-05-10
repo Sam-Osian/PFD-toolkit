4073399bd9a7d41418579d0b2df03d5699d7983e
Sam-Andrews <soandrews98@gmail.com>
2025-05-11 00:07:06 +0100

    Add Dataset class for filtering and retrieving PFD reports; bundle dataset into .toml

--- src/pfd_toolkit/loader.py
+++ src/pfd_toolkit/loader.py
@@ -1,5 +1,7 @@
 from pfd_toolkit import PFDScraper
 import pandas as pd
+from dateutil import parser as date_parser
+import importlib.resources as resources
 
 
 # -- INITALISE SCRAPER ENGINE --
@@ -22,3 +24,48 @@ reports = pd.read_csv('data/all_reports.csv')
 
 # Try to 'top up' with new ones
 scraper.top_up(old_reports=reports, date_from="2025-05-09")
+
+
+class Dataset:
+    """
+    Provides access to fully cleaned and ready-to-use Prevention of Future Death
+    (PFD) reports.
+    """
+
+    def __init__(
+        self,
+        category: str = 'all',
+        date_from: str = '2000-01-01',
+        date_to: str = '2100-01-01'
+    ) -> None:
+        """Initialises the dataset loader with specified configurations.
+
+        Args:
+            category (str, optional): The category of PFD reports. Defaults to 'all'.
+            date_from (str, optional): The start date for filtering PFD reports. Defaults to '2000-01-01'.
+            date_to (str, optional): The end date for filtering PFD reports. Defaults to '2100-01-01'.
+        """
+        
+        self.category = category.lower() # Normalise category to lowercase if user specifies otherwise
+        
+        # Parse date strings into datetime objects (for internal use)
+        self.date_from = date_parser.parse(date_from)
+        self.date_to = date_parser.parse(date_to)
+        
+        # Throw error if date_to is earlier than date_from
+        if self.date_from > self.date_to:
+            raise ValueError("date_from must be earlier than or equal to date_to.")
+        
+        
+    def get_data(self) -> pd.DataFrame:
+        # Retrive data from `data` directory
+        with resources.files('pfd_toolkit.data').joinpath('all_reports.csv').open('r') as f:
+            reports = pd.read_csv(f)
+
+        # Make Date column into pandas datetime obj
+        reports['Date'] = pd.to_datetime(reports['Date'])
+        
+        # Filter date based on user's specification
+        reports = reports[(reports['Date'] >= self.date_from) & (reports['Date'] <= self.date_to)]
+        
+        return reports
