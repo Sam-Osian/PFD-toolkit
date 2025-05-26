644e53ec389c8c44b7219bdddf53a8ca77d68b47
Sam-Andrews <soandrews98@gmail.com>
2025-05-26 21:00:06 +0100

    Replace all_reports.csv with full HTML+PDF+LLM cascading scrape for fuller extraction

---
+++ scripts/scrape_reports.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Helper script for scraping all PFD reports contained
+within `../data/full_reports.csv.
+"""
+
+from pfd_toolkit import LLM, PFDScraper
+from dotenv import load_dotenv 
+import os
+
+# Load OpenAI API key
+load_dotenv("../notebooks/api.env")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+llm_client = LLM(api_key=openai_api_key, max_workers=30)
+
+# Set up scraper
+scraper = PFDScraper(
+    llm = llm_client,
+    html_scraping=True,
+    pdf_fallback=True,
+    llm_fallback=True,
+    delay_range=None)
+
+# Run scraper
+scraper.scrape_reports()
+
+reports = scraper.reports
+
+#reports.to_csv('../src/pfd_toolkit/data/all_reports.csv')
