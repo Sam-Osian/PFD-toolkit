f68337a1bef5a44b2d68a657cca0cb1ce21c080a
Sam-Andrews <soandrews98@gmail.com>
2025-05-27 16:31:13 +0100

    Add static report counter to GitHub workflow & update documentation on commit

---
+++ scripts/update_report_count.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Helper script for retrieving the number of reports contained
+within the dataset bundled with PFD Toolkit.
+"""
+
+import pandas as pd
+
+csv_path = "src/pfd_toolkit/data/all_reports.csv"
+md_path = "docs/index.md"
+
+# Count number of rows/reports
+df = pd.read_csv(csv_path)
+count = len(df)
+
+# Replace {{NUM_REPORTS}} in docs/index.md
+with open(md_path, "r", encoding="utf-8") as f:
+    text = f.read()
+    
+text = text.replace("{{NUM_REPORTS}}", str(count))
+
+with open(md_path, "w", encoding="utf-8") as f:
+    f.write(text)
