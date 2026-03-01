6b8050f8120b86d32c48b9cef4d8e60094113354
Sam-Osian <soandrews98@gmail.com>
2026-03-01 22:26:40 +0000

    Tweak settings

--- django_workbench/pfd_workbench/settings.py
+++ django_workbench/pfd_workbench/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=not DEBUG,
+        ssl_require=_env_flag("DJANGO_DB_SSL_REQUIRE", False),
     )
 }
 
